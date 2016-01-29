434cd846e7987fafa6af8ea85cb296a534178097
izdislav <extralong@abv.bg>
2016-01-29 17:52:45 +0200

    new commit

--- parsemail.php
+++ parsemail.php
@@ -27,22 +27,22 @@ try
 	$email = $parsemail->getEmailDetails($savedEmailId);
 	Debug::write($email);
 	
-	$userEmailIds = $parsemail->getEmails("extralong@abv.bg");
+	$userEmailIds = $parsemail->getEmails("plamen@gmail.com");
 	Debug::write($userEmailIds);
 	
 	$emailsWithHeaderName = $parsemail->getEmailsByHeader("to");
 	Debug::write($emailsWithHeaderName);
 	
-	$userEmailCount = $parsemail->getEmails("extralong@abv.bg", false);
+	$userEmailCount = $parsemail->getEmails("plamen@gmail.com", false);
 	Debug::write($userEmailCount);
 	
 	$emailCount = $parsemail->getEmails("", true);
 	Debug::write($emailCount);
 	
-	$userEmailsPerSenderDomain = $parsemail->getEmailsPerSenderDomain("extralong@abv.bg");
+	$userEmailsPerSenderDomain = $parsemail->getEmailsPerSenderDomain("plamen@gmail.com");
 	Debug::write('$userEmailIds',$userEmailsPerSenderDomain);
 	
-	$emailSpfPassPercent = $parsemail->getEmailsSpfPass("extralong@abv.bg");
+	$emailSpfPassPercent = $parsemail->getEmailsSpfPass("plamen@gmail.com");
 	Debug::write('$userEmailIds',$emailSpfPassPercent);
 	 
 
